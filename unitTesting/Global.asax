39494639c54e35c13f0f3835182403f1ac8a95b6
BrindaGeorge <brindageorge94@gmail.com>
2017-08-22 18:45:05 +0530

    Add project files.

---
+++ unitTesting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="unitTesting.MvcApplication" Language="C#" %>
---
+++ unitTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(unitTesting.Startup))]
+namespace unitTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
